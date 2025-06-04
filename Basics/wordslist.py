bf6555733ba4b157fc949940649da697ea6c5db9
Gergely <gergely.kllr@gmail.com>
2025-06-04 22:15:10 +0200

    Hangman game, cleaning up

---
+++ Basics/wordslist.py
@@ -0,0 +1,3 @@
+# words for hangman game
+
+words = ("apple", "banana", "coconut", "orange", "pineapple", "grape", "kiwi", "lemon", "mango", "nectarine", "orange", "peach", "quince", "raspberry", "strawberry", "watermelon", "apple", "banana", "coconut", "orange", "pineapple", "grape", "kiwi", "lemon", "mango", "nectarine", "orange", "peach", "quince", "raspberry", "strawberry", "watermelon")

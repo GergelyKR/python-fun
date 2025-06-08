a6f01ce0ec06087521052553dfa6b316046178ba
Gergely Kollar <gergely.kollar@t-systems.com>
2025-06-08 15:05:52 +0200

    cs50p for loop

--- cs50p/2_loops.py
+++ cs50p/2_loops.py
@@ -2,3 +2,11 @@ i = 0
 while i < 3:
     print("meow")
     i += 1
+
+print()
+
+# for i in[0, 1, 2]:
+#     print("meow")
+
+for _ in range(3): # _ for variables that won't be used later
+    print("meow")

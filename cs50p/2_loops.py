449bff70c7eefb30c01def8aa9ea0e1b8e82bfd9
Gergely Kollar <gergely.kollar@t-systems.com>
2025-06-09 18:08:15 +0200

    cs50p input validation

--- cs50p/2_loops.py
+++ cs50p/2_loops.py
@@ -1,12 +1,31 @@
-i = 0
-while i < 3:
-    print("meow")
-    i += 1
+def main():
+        
+    i = 0
+    while i < 3:
+        print("meow")
+        i += 1
 
-print()
+    print()
 
-# for i in[0, 1, 2]:
-#     print("meow")
+    # for i in[0, 1, 2]:
+    #     print("meow")
 
-for _ in range(3): # _ for variables that won't be used later
-    print("meow")
+    for _ in range(3): # _ for variables that won't be used later
+        print("meow")
+
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True: # Keep looping until you get valid input
+        n = int(input("\nHow many meows? "))
+        if n > 0:
+            # break
+            return n
+        # return n
+    
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+main()

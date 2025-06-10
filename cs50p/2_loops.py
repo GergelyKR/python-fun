b2081cd41d0d0121dc291a2fa57b9ca45aaffa32
Gergely Kollar <gergely.kollar@t-systems.com>
2025-06-10 17:02:41 +0200

    cs50p lists

--- cs50p/2_loops.py
+++ cs50p/2_loops.py
@@ -7,14 +7,24 @@ def main():
 
     print()
 
-    # for i in[0, 1, 2]:
+    # for i in[0, 1, 2]: -> assigns i to 0, 1, 2 and runs the indented code every time
     #     print("meow")
 
-    for _ in range(3): # _ for variables that won't be used later
+    for _ in range(3): # _ for variables that won't be used later. Range will act the same way as "for i in[0, 1, 2]"
         print("meow")
 
     number = get_number()
     meow(number)
+
+    print()
+    # ---
+
+    students = ["Harry", "Hermione", "Ron"]
+    
+    # for i in range(students) -> Students is not a number, it's not an integer, range expects an integer.
+    for i in range(len(students)): # len will return the number of items as integer, so range works
+        print(i + 1, students[i])
+
 
 def get_number():
     while True: # Keep looping until you get valid input

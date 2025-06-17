0611b9a75489055a831d3d97a20b83ff82c36107
Gergely <gergely.kllr@gmail.com>
2025-06-17 22:58:09 +0200

    cs50p sys library

---
+++ cs50p/4b_sys.py
@@ -0,0 +1,12 @@
+import sys
+
+# Check for errors
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+# elif len(sys.argv) > 2:
+#     sys.exit("Too many arguments")
+
+# print(f"Hello, {sys.argv[1]}")
+
+for arg in sys.argv[1:]:
+    print("Hello, my name is", arg)

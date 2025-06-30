3961ee01f4d3ddd10958dd2c91481b325aa914bd
Gergely <gergely.kllr@gmail.com>
2025-06-30 23:39:06 +0200

    cs50p regex part 2

--- cs50p/7_validate.py
+++ cs50p/7_validate.py
@@ -6,10 +6,21 @@ import re
 # * = zero or more
 # ^ = start of the string
 # $ = end of the string
+# [] = a set of characters
+# [^] = exclude a set of characters
+# \w = word characters (a-z, A-Z, 0-9, _)
+# \W = non-word characters
+# \d = digits (0-9)
+# \D = non-digits
+# \s = whitespace (space, tab, newline)
+# \S = non-whitespace
+# A|B = either A or B
+# (...) = group
+# (?:...) = non-capturing group
 
 email = input("What's your email? ").strip() # .strip() removes spaces from the beginning and the end of the string
 
-if re.search(r"^.+@.+\.edu$", email):
+if re.search(r"^\w+@\w+\.edu$", email):
     print("Valid")
 else:
     print("Invalid")

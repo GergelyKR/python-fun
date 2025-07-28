2436f390f8cd85fc09f5062ce729b360e20915bd
Gergely Kollar <gergely.kollar@t-systems.com>
2025-07-28 15:16:43 +0200

    Snake finish

--- snake_game/main.py
+++ snake_game/main.py
@@ -30,6 +30,7 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
         
     # Detect collision with wall
@@ -37,6 +38,12 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.game_over()
         game_is_on = False
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
     
 
 screen.exitonclick()

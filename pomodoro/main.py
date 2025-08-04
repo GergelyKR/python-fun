dae7889b0a281f3980e70cad9cf9f309ce8d9aa0
Gergely <gergely.kllr@gmail.com>
2025-08-04 14:13:53 +0200

    html parser start

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,7 +1,5 @@
 from tkinter import *
 import math
-
-
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,19 +8,20 @@ YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+LONG_BREAK_MIN = 15
 reps = 0
 timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 def reset_timer():
-    window.after_cancel(timer) # cancel the window.after() later on
+    window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
     title_label.config(text="Timer")
     check_marks.config(text="")
     global reps
     reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -34,24 +33,26 @@ def start_timer():
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    count_down(work_sec)
     if reps % 8 == 0:
         count_down(long_break_sec)
-        title_label.config(text="Long Break", fg=RED)
+        title_label.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
-        title_label.config(text="Short Break", fg=PINK)
+        title_label.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
         title_label.config(text="Work", fg=GREEN)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
 
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
-    canvas.itemconfig(timer_text, text=f"{count_min:02d}:{count_sec:02d}")
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         global timer
         timer = window.after(1000, count_down, count - 1)
@@ -69,6 +70,7 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
 
@@ -84,7 +86,10 @@ start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
-window.mainloop() # to show the window and keep checking if an event happened
+
+
+
+window.mainloop()

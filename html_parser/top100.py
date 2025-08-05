69faad9c9638042fc03b44449b843eb475ce076a
Gergely <gergely.kllr@gmail.com>
2025-08-05 21:48:34 +0200

    Parsing 100 movies

---
+++ html_parser/top100.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h2")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[:2:-1]
+
+for movie in movies:
+    print(movie)
+
+# with open("movies.txt", mode="w") as file:
+#     for movie in movies:
+#         file.write(f"{movie}\n")
